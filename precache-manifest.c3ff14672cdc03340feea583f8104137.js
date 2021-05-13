07a09fa80e28d65194a04cf3ac8c8189f2dcc96f
Alex Fedorchenko <mralex.fed@gmail.com>
2021-05-13 20:15:58 +0300

    Updates

--- precache-manifest.c3ff14672cdc03340feea583f8104137.js
+++ precache-manifest.c3ff14672cdc03340feea583f8104137.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9cf895c55918099da7954224dce2b0dc",
+    "revision": "89962776cd73046e0e109e3a3df2fa4f",
     "url": "./index.html"
   },
   {
-    "revision": "53e7aa93e1d9a2a22692",
+    "revision": "5a44d4634c0e0d5fc9e2",
     "url": "./static/css/2.f6a27176.chunk.css"
   },
   {
-    "revision": "256c8fc7f50a1da388da",
+    "revision": "92faf7ea37bbc4e9ea28",
     "url": "./static/css/main.bada76a3.chunk.css"
   },
   {
-    "revision": "53e7aa93e1d9a2a22692",
-    "url": "./static/js/2.77914c2a.chunk.js"
+    "revision": "5a44d4634c0e0d5fc9e2",
+    "url": "./static/js/2.d87bbb2c.chunk.js"
   },
   {
     "revision": "570d362d673dab785e62d2b8563e1118",
-    "url": "./static/js/2.77914c2a.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.d87bbb2c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "256c8fc7f50a1da388da",
-    "url": "./static/js/main.ed9ec9d0.chunk.js"
+    "revision": "92faf7ea37bbc4e9ea28",
+    "url": "./static/js/main.97421619.chunk.js"
   },
   {
     "revision": "79d17474afd20bce64b6",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a5c8e7582e9710b85eb248c87fb4946a.js"
+  "./precache-manifest.c3ff14672cdc03340feea583f8104137.js"
 );
 
 self.addEventListener('message', (event) => {
